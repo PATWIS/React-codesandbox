830ee3f3f461a1dabcd10e8cc4721e935f16337b
Patryk Wiśniewski <patryk.wisniewski@outlook.com>
2018-04-09 17:04:24 +0200

    refactor

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,20 +3,25 @@ import { withStyles } from "material-ui/styles";
 import Paper from "material-ui/Paper";
 import Typography from "material-ui/Typography";
 import Button from "material-ui/Button";
+import Grid from "material-ui/Grid";
+import Divider from "material-ui/Divider";
 
 const styles = theme => ({
   root: {
     flex: 1
   },
   button: {
-    margin: 20
+    width: 100,
+    marginTop: 15
   },
   paper: {
-    height: 150,
-    marginTop: 20
-  },
-  title: {
+    minHeight: 150,
+    marginTop: 20,
     padding: 20
+  },
+  link: {
+    color: "inherit",
+    textDecoration: "none"
   }
 });
 
@@ -31,27 +36,39 @@ class Item extends React.Component {
   };
 
   render() {
-    const { classes, data } = this.props;
+    const { classes, data, login } = this.props;
     return (
       <Paper className={classes.paper}>
-        <Typography className={classes.title} variant="title" gutterBottom>
-          Title: {data.title}
-        </Typography>
-        <Button
-          variant="raised"
-          onClick={this.goToEdit}
-          className={classes.button}
-        >
-          Edit
-        </Button>
-        <Button
-          variant="raised"
-          color="secondary"
-          onClick={this._handleDelete.bind(this, data)}
-          className={classes.button}
-        >
-          Delete
-        </Button>
+        <Grid container spacing={12} className={classes.root}>
+          <Grid item xs={login ? 9 : 12}>
+            <Typography variant="title" gutterBottom>
+              {data.name}
+            </Typography>
+            <Typography variant="subheading" gutterBottom>
+              {data.desc}
+            </Typography>
+          </Grid>
+
+          {login && (
+            <Grid container xs={3} direction="column" alignItems="flex-end">
+              <Button
+                variant="raised"
+                onClick={this.goToEdit}
+                className={classes.button}
+              >
+                Edit
+              </Button>
+              <Button
+                variant="raised"
+                color="secondary"
+                onClick={this._handleDelete.bind(this, data)}
+                className={classes.button}
+              >
+                Delete
+              </Button>
+            </Grid>
+          )}
+        </Grid>
       </Paper>
     );
   }
